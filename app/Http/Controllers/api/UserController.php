b32b699bab1a16e34700a31a53d580a29a6dc916
Rival <90138726+emRival@users.noreply.github.com>
2024-05-04 18:27:32 +0300

    add sanctum and api login, get products

---
+++ app/Http/Controllers/api/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+
+
+    public function loginuser(Request $request)
+    {
+        //check if user exists
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        //check if password is correct
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Password is incorrect'
+            ], 401);
+        }
+
+        //generate token
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'login successful',
+            'status' => '200',
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\api\ProductController;
+use App\Http\Controllers\api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', [UserController::class, 'loginuser']);
+
+Route::apiResource('api-products', ProductController::class)->middleware('auth:sanctum');
